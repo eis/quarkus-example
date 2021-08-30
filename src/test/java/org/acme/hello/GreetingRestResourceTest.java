6ce970ebff37a57c0c08473fe0e59c4fe7c41f46
eis <eis@iki.fi>
2021-08-30 13:43:21 +0300

    cover error cases too

--- src/test/java/org/acme/hello/GreetingRestResourceTest.java
+++ src/test/java/org/acme/hello/GreetingRestResourceTest.java
@@ -4,6 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
+import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -16,6 +18,28 @@ public class GreetingRestResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from Quarkus"));
+    }
+    @Test
+    public void test500Request() {
+        given()
+            .log().ifValidationFails()
+            .when()
+            .get("/500")
+            .then()
+            .log().ifValidationFails()
+            .statusCode(SC_INTERNAL_SERVER_ERROR)
+            .body(is("Internal error from Quarkus"));
+    }
+    @Test
+    public void test400Request() {
+        given()
+            .log().ifValidationFails()
+            .when()
+            .get("/400")
+            .then()
+            .log().ifValidationFails()
+            .statusCode(SC_BAD_REQUEST)
+            .body(is("Bad request from Quarkus"));
     }
 
 }
